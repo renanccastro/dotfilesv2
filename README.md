106bba7f4dc076a893c65bd2a5bf2654d254ac6f
Renan <renanccastro@gmail.com>
2014-01-27 19:59:30 -0200

    install script

---
+++ README.md
@@ -0,0 +1,4 @@
+This dotfiles uses spf13 and oh-my-zsh
+
+
+so, just run the install.sh script to install the required dependencies.
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+
+ln -sf * ~/
