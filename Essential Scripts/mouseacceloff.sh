c052c3ae12fb2c4a53440ef59f1ee2d2fe207e85
Renan <renanccastro@gmail.com>
2014-01-28 19:39:53 -0200

    Adding scripts and updating things.

---
+++ Essential Scripts/mouseacceloff.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ 
+xinput set-prop 'USB OPTICAL MOUSE' 'Device Accel Profile' -1
+xinput set-prop 'USB OPTICAL MOUSE' 'Device Accel Constant Deceleration' 2 
+
--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 
 ln -sf * ~/
+ln -sf gtkthemes/* /usr/share/themes/
+
